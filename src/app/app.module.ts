e7aa83c11b16d3a6c977b11600f7a8fd8c2acbaa
mukhammadsaid19 <m.mamasaidov@student.inha.uz>
2021-04-17 10:20:44 +0500

    setup finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AnnotatorComponent } from './annotator/annotator.component';
+import { TestComponent } from './annotator/test/test.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnnotatorComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
